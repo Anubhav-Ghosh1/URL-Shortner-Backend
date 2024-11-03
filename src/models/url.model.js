cfdff5f1b64d003022619fa0bf97d355be7c939d
Anubhav-Ghosh1 <iamanubhav11@gmail.com>
2024-11-03 10:37:38 +0530

    Added Code for User Controller

--- src/models/url.model.js
+++ src/models/url.model.js
@@ -11,11 +11,36 @@ const URLSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    userId:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
     visitorCount:
     {
         type: Number,
         required: true,
-    }
+    },
+    isActive:
+    {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+    customAlias: {
+        type: String,
+        required: false,
+        unique: true,
+        sparse: true, // Only apply uniqueness constraint when alias is present
+    },
+    visitorDetails: [{
+        ipAddress: String,
+        userAgent: String,
+        accessedAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }]
 },{new: true});
 
 export const URL = mongoose.model("URL",URLSchema);
