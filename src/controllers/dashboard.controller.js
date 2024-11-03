0634bf967b5ec8b83930635d0a03110af56b58a2
Anubhav-Ghosh1 <iamanubhav11@gmail.com>
2024-11-03 14:04:29 +0530

    Added Dashboard Controller

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,60 @@
+import { User } from "../models/user.model";
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const userAnalytics = asyncHandler(async (req,res) => {
+    try
+    {
+        const id = req?.user?._id;
+        if(!id)
+        {
+            return res.status(400).json(new ApiResponse(400,{},"All fields are required"));
+        }
+
+        const response = await User.aggregate([
+            {
+                $match:
+                {
+                    _id: id,
+                }
+            },
+            {
+                $lookup:
+                {
+                    from: "urls",
+                    foreignField: "userId",
+                    localField: "_id",
+                    as: "Url",
+                }
+            },
+            {
+                $addFields: {
+                    totalVisitorCount: { $sum: "$Url.visitorCount" }, // Sum of all visitor counts
+                    totalUrls: { $size: "$Url" } // Count of total URLs
+                }
+            },
+            {
+                $project:
+                {
+                    username: 1,
+                    totalVisitorCount: 1,
+                    totalUrls: 1,
+                    urls: {
+                        shortnedURL: 1,
+                        originalURL: 1,
+                        visitorCount: 1,
+                        isActive: 1
+                    }
+                }
+            }
+        ]);
+
+        return res.status(200).json(new ApiResponse(200,response,"User details fetched successfully"));
+    }
+    catch(e)
+    {
+        return res.status(500).json(new ApiResponse(500,{},"Error while fetching details"));
+    }
+});
+
+export {userAnalytics};
